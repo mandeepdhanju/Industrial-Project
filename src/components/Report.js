2b1a41352d14b9931a639f42dca994e33ff7bbc9
Ryan Lim <ryan.hyun@gmail.com>
2022-05-03 13:23:57 -0700

    add download

--- src/components/Report.js
+++ src/components/Report.js
@@ -1,3 +1,4 @@
+import CsvDownload from "react-json-to-csv";
 import { useEffect, useState } from "react";
 const axios = require("axios");
 const path = "https://localhost:5001/api/Search/all";
@@ -6,16 +7,46 @@ function Report() {
   const [all, setAll] = useState();
   const [columnName, setColumnName] = useState([]);
   const [checkedArray, setCheckedArray] = useState([]);
+  const [exportArray, setExportArray] = useState([]);
 
   useEffect(() => {
     async function getAll() {
       await axios.get(path).then((response) => {
         setAll(response.data);
-        setColumnName(Object.keys(response.data[0]));
+        // setColumnName(Object.keys(response.data[0]));
+        let columns = Object.keys(response.data[0]);
+        columns.forEach((item, index) => {
+          if (item.includes("ID")) {
+            columns.splice(index, 1);
+          }
+        });
+        setColumnName(columns);
       });
     }
     getAll();
   }, []);
+
+  useEffect(() => {
+    function download() {
+      console.log("hello");
+      let exportData = [];
+      var obj = {};
+      if (checkedArray.length !== 0) {
+        for (let i = 0; all.length > i; i++) {
+          for (let j = 0; checkedArray.length > j; j++) {
+            let newObj = {};
+            let item = all[i][checkedArray[j]];
+            newObj[checkedArray[j]] = item;
+            Object.assign(obj, newObj);
+          }
+          exportData.push(obj);
+          console.log(exportData);
+        }
+        setExportArray(exportData);
+      }
+    }
+    download();
+  }, [checkedArray]);
 
   function checkedColumn(checkedColumn) {
     if (checkedArray.includes(checkedColumn)) {
@@ -27,16 +58,6 @@ function Report() {
     } else {
       setCheckedArray((checked) => [...checked, checkedColumn]);
     }
-  }
-  let downloadArray = [];
-  function downloadPressed() {
-    for (let i = 0; all.length > i; i++) {
-      for (let j = 0; checkedArray.length > j; j++) {
-        // downloadArray.push(all[i].checkedArray[j]);
-        console.log(all[i][checkedArray[j]]);
-      }
-    }
-    // console.log(downloadArray);
   }
 
   return (
@@ -56,9 +77,7 @@ function Report() {
           ))}
       </div>
       <div className="reportPreview">
-        <button className="reportDownloadButton" onClick={downloadPressed}>
-          Download
-        </button>
+        <CsvDownload data={exportArray}>Download</CsvDownload>
         <h1>PREVIEW</h1>
         <table>
           <tr>{checkedArray && checkedArray.map((i) => <th>{i}</th>)}</tr>
