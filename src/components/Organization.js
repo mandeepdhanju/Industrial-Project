ff2a4c10ffcc4a5701972dae687060d68b47c1c9
Ryan Lim <ryan.hyun@gmail.com>
2022-05-02 21:12:12 -0700

    on downloadpress button

--- src/components/Organization.js
+++ src/components/Organization.js
@@ -1,79 +1,117 @@
-import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
-import OrganizationCreateForm from "./OrganizationCreateForm"
-import OrganizationUpdateForm from "./OrganizationUpdateForm"
-const axios = require('axios')
-const path = "https://localhost:5001/api/community"
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import OrganizationCreateForm from "./OrganizationCreateForm";
+import OrganizationUpdateForm from "./OrganizationUpdateForm";
+const axios = require("axios");
+const path = "https://localhost:5001/api/";
 
 function Organization() {
-  const [orgs, setOrgs] = useState([])
-  const [orgObj, setOrgObj] = useState({})
-  const [edit, setEdit] = useState(false)
-  const [create, setCreate] = useState(false)
+  const [orgs, setOrgs] = useState([]);
+  const [orgObj, setOrgObj] = useState({});
+  const [edit, setEdit] = useState(false);
+  const [create, setCreate] = useState(false);
 
   let navigate = useNavigate();
 
   useEffect(() => {
     async function getAllOrgs() {
-      const response = await axios.get(path + "Organization")
-      setOrgs(response.data)
+      const response = await axios.get(path + "Organization");
+      setOrgs(response.data);
     }
-    getAllOrgs()
-  }, [])
+    getAllOrgs();
+  }, []);
 
   function getBranchInfo(id) {
-    let path = "/organization/" + id
-    navigate(path)
+    let path = "/organization/" + id;
+    navigate(path);
   }
 
   async function markInactive(id) {
-    const response = await axios.delete(path + "Organization/" + id)
-    console.log(response)
+    const response = await axios.delete(path + "Organization/" + id);
+    console.log(response);
 
     //update again
   }
 
   function pushToArray(newOrgObj) {
-    setOrgs([...orgs, newOrgObj])
+    setOrgs([...orgs, newOrgObj]);
   }
 
-  return <div className="organization">
-    {create ? <OrganizationCreateForm pushToArray={pushToArray}></OrganizationCreateForm> : ""}
-    <button onClick={() => setCreate(!create)}>Create New Organization</button>
-    <table>
-      <thead><tr>
-        <th className="t-num">#</th>
-        <th className="t-name">Organization</th>
-        <th>Category</th>
-        <th>SubCategory</th>
-        <th>Number of Active Branches</th>
-        <th>Number of Employees</th>
-        <th>Website</th>
-        <th className="t-actions">Actions</th>
-      </tr></thead>
-
-      {orgs.map((org, index) => {
-        return <tbody key={(org.organizationID)}>
-
-          <tr onClick={() => { getBranchInfo(org.organizationID) }}>
-            <td>{index + 1}</td>
-            <td>{org.organizationName}</td>
-            <td>{org.category}</td>
-            <td>{org.subCategory}</td>
-            <td>{org.activeBranches}</td>
-            <td>{org.numberOfEmployees}</td>
-            <td onClick={(e) => e.stopPropagation()}>{org.website ? <a href={org.website}>Website</a> : ""}</td>
-            <td>
-              <button onClick={(e) => { e.stopPropagation(); setEdit(!edit); setOrgObj(org) }}>✏️</button>
-              <button onClick={(e) => { e.stopPropagation(); markInactive(org.organizationID) }}>{org.active ? "🟢" : "🔴"}</button>
-            </td>
+  return (
+    <div className="organization">
+      {create ? (
+        <OrganizationCreateForm
+          pushToArray={pushToArray}
+        ></OrganizationCreateForm>
+      ) : (
+        ""
+      )}
+      <button onClick={() => setCreate(!create)}>
+        Create New Organization
+      </button>
+      <table>
+        <thead>
+          <tr>
+            <th className="t-num">#</th>
+            <th className="t-name">Organization</th>
+            <th>Category</th>
+            <th>SubCategory</th>
+            <th>Number of Active Branches</th>
+            <th>Number of Employees</th>
+            <th>Website</th>
+            <th className="t-actions">Actions</th>
           </tr>
-        </tbody>
-      })}
-    </table>
+        </thead>
 
-    {edit ? <OrganizationUpdateForm orgObj={orgObj}></OrganizationUpdateForm> : ""}
-  </div >
+        {orgs.map((org, index) => {
+          return (
+            <tbody key={org.organizationID}>
+              <tr
+                onClick={() => {
+                  getBranchInfo(org.organizationID);
+                }}
+              >
+                <td>{index + 1}</td>
+                <td>{org.organizationName}</td>
+                <td>{org.category}</td>
+                <td>{org.subCategory}</td>
+                <td>{org.activeBranches}</td>
+                <td>{org.numberOfEmployees}</td>
+                <td onClick={(e) => e.stopPropagation()}>
+                  {org.website ? <a href={org.website}>Website</a> : ""}
+                </td>
+                <td>
+                  <button
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      setEdit(!edit);
+                      setOrgObj(org);
+                    }}
+                  >
+                    ✏️
+                  </button>
+                  <button
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      markInactive(org.organizationID);
+                    }}
+                  >
+                    {org.active ? "🟢" : "🔴"}
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          );
+        })}
+      </table>
+
+      {edit ? (
+        <OrganizationUpdateForm orgObj={orgObj}></OrganizationUpdateForm>
+      ) : (
+        ""
+      )}
+    </div>
+  );
 }
 
 export default Organization;
--- src/components/Report.js
+++ src/components/Report.js
@@ -1,5 +1,80 @@
+import { useEffect, useState } from "react";
+const axios = require("axios");
+const path = "https://localhost:5001/api/Search/all";
+
 function Report() {
-  return <div className="report">hello from Report</div>;
+  const [all, setAll] = useState();
+  const [columnName, setColumnName] = useState([]);
+  const [checkedArray, setCheckedArray] = useState([]);
+
+  useEffect(() => {
+    async function getAll() {
+      await axios.get(path).then((response) => {
+        setAll(response.data);
+        setColumnName(Object.keys(response.data[0]));
+      });
+    }
+    getAll();
+  }, []);
+
+  function checkedColumn(checkedColumn) {
+    if (checkedArray.includes(checkedColumn)) {
+      const index = checkedArray.indexOf(checkedColumn);
+      setCheckedArray([
+        ...checkedArray.slice(0, index),
+        ...checkedArray.slice(index + 1),
+      ]);
+    } else {
+      setCheckedArray((checked) => [...checked, checkedColumn]);
+    }
+  }
+  let downloadArray = [];
+  function downloadPressed() {
+    for (let i = 0; all.length > i; i++) {
+      for (let j = 0; checkedArray.length > j; j++) {
+        // downloadArray.push(all[i].checkedArray[j]);
+        console.log(all[i][checkedArray[j]]);
+      }
+    }
+    // console.log(downloadArray);
+  }
+
+  return (
+    <div className="report">
+      <div className="columnSelection">
+        {columnName &&
+          columnName.map((item) => (
+            <div>
+              <input
+                type="checkbox"
+                onChange={() => {
+                  checkedColumn(item);
+                }}
+              />
+              <label htmlFor="columnName">{item}</label>
+            </div>
+          ))}
+      </div>
+      <div className="reportPreview">
+        <button className="reportDownloadButton" onClick={downloadPressed}>
+          Download
+        </button>
+        <h1>PREVIEW</h1>
+        <table>
+          <tr>{checkedArray && checkedArray.map((i) => <th>{i}</th>)}</tr>
+
+          {all &&
+            all.map((row) => (
+              <tr>
+                {checkedArray.map((column) => (
+                  <td>{row[column]}</td>
+                ))}
+              </tr>
+            ))}
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default Report;
