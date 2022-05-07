236023eeecdfccab6a16a14501a4cc9efd8bb6f1
Ryan Lim <ryan.hyun@gmail.com>
2022-05-07 12:18:31 -0700

    click column name also work

--- src/components/Report.js
+++ src/components/Report.js
@@ -113,28 +113,32 @@ function Report() {
   return (
     <div className="report">
       <div className="columnSelection">
-        <button onClick={selectAllFunc}>Select All</button>
-        <button onClick={deselect}>Deselect All</button>
+        <button id="allSB" onClick={selectAllFunc}>
+          Select All
+        </button>
+        <button id="allDSB" onClick={deselect}>
+          Deselect All
+        </button>
         {columnName &&
           columnName.map((item) => (
-            <div>
+            <div
+              onClick={() => {
+                checkedColumn(item);
+              }}
+            >
               <input
                 checked={checkedArray.includes(item) ? true : false}
-
                 type="checkbox"
-                onChange={() => {
-                  checkedColumn(item);
-                }}
               />
               <label htmlFor="columnName">{item}</label>
-              
-              
             </div>
           ))}
       </div>
 
       <div className="reportPreview">
-        <CsvDownload data={exportArray} className="download">Download</CsvDownload>
+        <CsvDownload data={exportArray} className="download">
+          Download
+        </CsvDownload>
         <h1>PREVIEW</h1>
         <table>
           <tr>
